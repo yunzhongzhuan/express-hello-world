f5756039f2759d0d5393d9edb1ed46605ddf8fdc
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-02-02 15:17:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  // console.log(`Example app listening at http://localhost:${port}`)
 })
