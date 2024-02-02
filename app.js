97c30e08edde27ae6cff1660e90cae8df93e7bc1
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-02-02 15:21:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(function (req, res, next) {
   //res.set('x-timestamp', Date.now())
   //res.set('x-powered-by', 'cyclic.sh')
+  res.set('req-json',req..toISOString());
   //console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
   next();
 });
